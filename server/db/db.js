310d0e09e8a5a36da1a524f42cc49e57f12a2bcc
Nataliia Baikina <nataliiabaikina@Nataliias-MBP.home>
2017-12-14 23:26:15 -0500

    deploy

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
-import config from '../config/config.json';
+import config from '../../config/config.json';
 
 export function setUpConnection() {
-	mongoose.connect(`mongodb://${config.user}:${config.user}@ds133746.mlab.com:${config.user}/${config.name}`);
+	mongoose.connect(`mongodb://${config.db.user}:${config.db.password}@ds133746.mlab.com:${config.db.port}/${config.db.name}`);
 }
--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ import { serverPort } from '../config/config.json';
 const app = express();
 
 // Set up connection of database
-import mongoose from 'mongoose';
-const User = mongoose.model('User');
+// import mongoose from 'mongoose';
+// const User = mongoose.model('User');
 db.setUpConnection();
 
 // Using bodyParser middleware
