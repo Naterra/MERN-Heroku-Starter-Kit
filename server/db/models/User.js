e829da56a656af0b3dcfd8cd78f4df11a2f24505
Naterra <natalinyweb@hotmail.com>
2017-12-14 20:23:48 -0500

    update

---
+++ server/db/models/User.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { type: String },
+  email: { type: String },
+  country: { type: String },
+  address: { type: String },
+  state: { type: String },
+  zip: { type: String },
+  cell_phone: { type: String }
+});
+
+mongoose.model('User', UsertSchema);
